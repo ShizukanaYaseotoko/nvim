d3420651cccbb8b91854aaf5243a5e4219f4ad13
ShizukanaOtaku <otakushizukana@gmail.com>
2025-03-31 17:21:10 +0200

    add barbar

---
+++ lua/plugins/barbar.lua
@@ -0,0 +1,11 @@
+return {
+    "romgrk/barbar.nvim",
+    opts = {
+        auto_hide = 1,
+        animation = false,
+        tabpages = false,
+        clickable = false,
+        insert_at_end = true,
+        focus_on_close = 'right',
+    }
+}
