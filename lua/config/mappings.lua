6e710a28f91c2ff1f9e0f9e539d473a419449e79
ShizukanaOtaku <otakushizukana@gmail.com>
2025-03-31 15:52:28 +0200

    fix persistence xD

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -4,3 +4,6 @@ vim.keymap.set("n", "<leader>ff", function() Snacks.picker.files() end)
 
 -- Colorscheme picker
 vim.keymap.set("n", "<leader>ut", function() Snacks.picker.colorschemes() end)
+
+-- Persistence
+vim.keymap.set("n", "<leader>pr", function() require("persistence").load({ last = true }) end)
