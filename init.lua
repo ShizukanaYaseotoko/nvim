dbbb13111a5887c74f4f668332e643856ff35e6f
ShizukanaOtaku <otakushizukana@gmail.com>
2025-03-31 17:07:56 +0200

    configure blink, add options.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
+require("config.options")
 require("config.lazy")
+
 require("config.mappings")
 
 require("config.restore-colorscheme")
--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,30 +1,31 @@
 return {
-  'neovim/nvim-lspconfig',
-  dependencies = { 'saghen/blink.cmp' },
+  'saghen/blink.cmp',
+  dependencies = { 'rafamadriz/friendly-snippets' },
 
+  version = '*',
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
   opts = {
-    servers = {
-      lua_ls = {}
-    },
-    keymap = { preset = 'default' },
-
+    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+    -- 'super-tab' for mappings similar to vscode (tab to accept)
+    -- 'enter' for enter to accept
+    -- 'none' for no mappings
+    --
+    -- All presets have the following mappings:
+    -- C-space: Open menu or open docs if already open
+    -- C-n/C-p or Up/Down: Select next/previous item
+    -- C-e: Hide menu
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    keymap = { preset = 'enter' },
     appearance = {
-      nerd_font_variant = 'mono'
+      nerd_font_variant = 'mono',
     },
-
     completion = { documentation = { auto_show = true } },
-
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
     },
-
-    fuzzy = { implementation = "prefer_rust_with_warning" }
+    fuzzy = { implementation = "prefer_rust" },
+    signature = { enabled = true }
   },
-  config = function(_, opts)
-    local lspconfig = require('lspconfig')
-    for server, config in pairs(opts.servers) do
-      config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
-      lspconfig[server].setup(config)
-    end
-  end
+  opts_extend = { "sources.default" }
 }
