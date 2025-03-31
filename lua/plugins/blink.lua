4b0785cff9cdc8e4b72530108289cf0a1cdbf864
ShizukanaOtaku <otakushizukana@gmail.com>
2025-03-31 17:15:13 +0200

    set indentation to 4

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,31 +1,31 @@
 return {
-  'saghen/blink.cmp',
-  dependencies = { 'rafamadriz/friendly-snippets' },
+    'saghen/blink.cmp',
+    dependencies = { 'rafamadriz/friendly-snippets' },
 
-  version = '*',
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-    -- 'super-tab' for mappings similar to vscode (tab to accept)
-    -- 'enter' for enter to accept
-    -- 'none' for no mappings
-    --
-    -- All presets have the following mappings:
-    -- C-space: Open menu or open docs if already open
-    -- C-n/C-p or Up/Down: Select next/previous item
-    -- C-e: Hide menu
-    -- C-k: Toggle signature help (if signature.enabled = true)
-    keymap = { preset = 'enter' },
-    appearance = {
-      nerd_font_variant = 'mono',
+    version = '*',
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+        -- 'super-tab' for mappings similar to vscode (tab to accept)
+        -- 'enter' for enter to accept
+        -- 'none' for no mappings
+        --
+        -- All presets have the following mappings:
+        -- C-space: Open menu or open docs if already open
+        -- C-n/C-p or Up/Down: Select next/previous item
+        -- C-e: Hide menu
+        -- C-k: Toggle signature help (if signature.enabled = true)
+        keymap = { preset = 'enter' },
+        appearance = {
+            nerd_font_variant = 'mono',
+        },
+        completion = { documentation = { auto_show = true, auto_show_delay_ms = 0 } },
+        sources = {
+            default = { 'lsp', 'path', 'snippets', 'buffer' },
+        },
+        fuzzy = { implementation = "prefer_rust" },
+        signature = { enabled = true }
     },
-    completion = { documentation = { auto_show = true } },
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
-    fuzzy = { implementation = "prefer_rust" },
-    signature = { enabled = true }
-  },
-  opts_extend = { "sources.default" }
+    opts_extend = { "sources.default" }
 }
