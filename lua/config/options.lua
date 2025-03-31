b4a0892e7ff330cc43755ee069dd74c468991836
ShizukanaOtaku <otakushizukana@gmail.com>
2025-03-31 20:16:07 +0200

    add virtual text

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,11 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.wo.number = true
 vim.wo.relativenumber = true
+
+vim.diagnostic.config({
+    virtual_text = true,
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+})
