404504b5af7c9459bb3e5cdb333866132bb96582
ShizukanaOtaku <otakushizukana@gmail.com>
2025-03-31 18:12:53 +0200

    autocmds

--- init.lua
+++ init.lua
@@ -1,10 +1,11 @@
 require("config.options")
 require("config.lazy")
+require("config.autocmds.restore-colorscheme")
+require("config.autocmds.format-on-write")
 
 require("config.mappings")
 
-require("config.autocmds.restore-colorscheme")
-require("config.autocmds.format-on-write")
 require("lualine").setup()
 require("mason").setup()
 require("lsp-auto-setup").setup()
+require("config.autocmds.lsp-progress-notify")
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,10 +1,10 @@
-return { 
+return {
     "folke/snacks.nvim",
     opts = {
         dashboard = {
             sections = {
                 { section = "header" },
-                { section = "keys", gap = 1, padding = 1 },
+                { section = "keys",   gap = 1, padding = 1 },
                 { section = "startup" },
             },
             notifier = {},
