977c2d93ca07734f0bc810fb579ebb18d2cdec31
ShizukanaOtaku <otakushizukana@gmail.com>
2025-03-31 17:57:12 +0200

    format on save autocmd

--- init.lua
+++ init.lua
@@ -3,7 +3,8 @@ require("config.lazy")
 
 require("config.mappings")
 
-require("config.restore-colorscheme")
+require("config.autocmds.restore-colorscheme")
+require("config.autocmds.format-on-write")
 require("lualine").setup()
 require("mason").setup()
 require("lsp-auto-setup").setup()
