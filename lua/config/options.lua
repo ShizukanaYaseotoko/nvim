8a775ccad76bcae7c14a4d60dba09cf25a713a9f
ShizukanaOtaku <otakushizukana@gmail.com>
2025-04-01 20:24:47 +0200

    diagnostic signs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -15,7 +15,14 @@ vim.opt.foldenable = true
 
 vim.diagnostic.config({
     virtual_text = true,
-    signs = true,
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN] = "⚠",
+            [vim.diagnostic.severity.HINT] = "",
+            [vim.diagnostic.severity.INFO] = "",
+        },
+    },
     underline = true,
     update_in_insert = false,
     severity_sort = true,
