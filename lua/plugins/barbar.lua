83128094592c9e1ce15166f128635f6d11d9e118
ShizukanaOtaku <otakushizukana@gmail.com>
2025-04-01 15:48:42 +0200

    make barbar clickable

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -5,7 +5,6 @@ return {
         auto_hide = 1,
         animation = false,
         tabpages = false,
-        clickable = false,
         insert_at_end = true,
         focus_on_close = 'right',
     }
