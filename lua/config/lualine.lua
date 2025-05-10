77c7aaa2dd2a8aa9bbfb459a2dd71b757c2b5d7a
ShizukanaYaseotoko <otakushizukana@gmail.com>
2025-05-10 15:39:47 +0200

    neko -> mode

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -83,7 +83,7 @@ ins_left {
 ins_left {
     -- mode component
     function()
-        return '猫'
+        return 'モード'
     end,
     color = function()
         -- auto change color according to neovims mode
