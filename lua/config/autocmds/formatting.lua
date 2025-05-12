4e03f28a73891523c5236a2d01d5f57ee13b064b
ShizukanaYaseotoko <otakushizukana@gmail.com>
2025-05-12 13:56:12 +0200

    auto indent on write

--- lua/config/autocmds/formatting.lua
+++ lua/config/autocmds/formatting.lua
@@ -10,13 +10,14 @@ local function toggle_lsp_formatting()
 end
 
 vim.api.nvim_create_user_command("ToggleFormatting", toggle_lsp_formatting,
-    { desc = "Toggle the automatic LSP formatting" })
+{ desc = "Toggle the automatic LSP formatting" })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
     pattern = "*",
     callback = function()
         if vim.g.lsp_formatting_enabled then
             vim.lsp.buf.format()
+            vim.cmd("normal! mmgg=G`m")
         end
     end
 })
