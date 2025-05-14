bcda215be94ec1238650ffa5322f10308116b50a
ShizukanaYaseotoko <otakushizukana@gmail.com>
2025-05-14 21:58:12 +0200

    rm cyberdream and add a rounded border to lazy

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,9 +1,8 @@
 return {
-    { "folke/tokyonight.nvim",         name = "Tokyonight Color Scheme" },
-    { "Shatur/neovim-ayu",             name = "Ayu Color Scheme" },
-    { "catppuccin/nvim",               name = "Catppuccin Color Scheme" },
-    { "scottmckendry/cyberdream.nvim", name = "Cyberdream Color Scheme" },
-    { "projekt0n/github-nvim-theme",   name = "GitHub Color Scheme" },
-    { "rebelot/kanagawa.nvim",         name = "Kanagawa Color Scheme" },
-    { "rose-pine/neovim",              name = "Rose Pine Color Scheme" }
+    { "folke/tokyonight.nvim",       name = "Tokyonight Color Scheme" },
+    { "Shatur/neovim-ayu",           name = "Ayu Color Scheme" },
+    { "catppuccin/nvim",             name = "Catppuccin Color Scheme" },
+    { "projekt0n/github-nvim-theme", name = "GitHub Color Scheme" },
+    { "rebelot/kanagawa.nvim",       name = "Kanagawa Color Scheme" },
+    { "rose-pine/neovim",            name = "Rose Pine Color Scheme" }
 }
