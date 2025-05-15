52e09f1c4f8882a524c9cfae17205c8b79a13b27
ShizukanaYaseotoko <otakushizukana@gmail.com>
2025-05-15 16:44:29 +0200

    not only LSP, actually

--- lua/config/autocmds/formatting.lua
+++ lua/config/autocmds/formatting.lua
@@ -3,14 +3,14 @@ vim.g.lsp_formatting_enabled = true
 local function toggle_lsp_formatting()
     vim.g.lsp_formatting_enabled = not vim.g.lsp_formatting_enabled
     if vim.g.lsp_formatting_enabled then
-        print("LSP formatting enabled")
+        print("Formatting enabled")
     else
-        print("LSP formatting disabled")
+        print("Formatting disabled")
     end
 end
 
 vim.api.nvim_create_user_command("ToggleFormatting", toggle_lsp_formatting,
-{ desc = "Toggle the automatic LSP formatting" })
+{ desc = "Toggle the automatic formatting" })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
     pattern = "*",
