1c1e8842ed93d63ef3315a74f8f6be36a0796e05
ShizukanaYaseotoko <otakushizukana@gmail.com>
2025-05-23 15:34:48 +0200

    add gruvbox

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -4,5 +4,6 @@ return {
     { "catppuccin/nvim",             name = "Catppuccin Color Scheme" },
     { "projekt0n/github-nvim-theme", name = "GitHub Color Scheme" },
     { "rebelot/kanagawa.nvim",       name = "Kanagawa Color Scheme" },
-    { "rose-pine/neovim",            name = "Rose Pine Color Scheme" }
+    { "rose-pine/neovim",            name = "Rose Pine Color Scheme" },
+    { "ellisonleao/gruvbox.nvim",    name = "Gruvbox Color Scheme" }
 }
